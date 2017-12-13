5e87b7b1938a7c8a00dd41db6db81d9328b8a3b6
GreaterCode <1967368231@qq.com>
2017-12-14 00:47:55 +0800

    使用 truncatechars 模板过滤器自动提取文章摘要
    增加了一种自动提取文章摘要的方法：
    使用模板过滤器truncatechars:
    但是若采用该方法，对于html标签会比较难看

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -2,10 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils.six import python_2_unicode_compatible
+from django.utils.html import strip_tags
+import markdown
 
 # Create your models here.
 
-# python_2_unicode_compatible 用于金融港python2
+# python_2_unicode_compatible 用于python2
 @python_2_unicode_compatible
 class Category(models.Model):
 	def __str__(self):
@@ -22,19 +24,16 @@ class Tag(models.Model):
 
 @python_2_unicode_compatible
 class Post(models.Model):
-	def __str__(self):
-		return self.title
-	# 自定义 get_absolute_url 方法
-    # 记得从 django.urls 中导入 reverse 函数
-	def get_absolute_url(self):
-		return reverse('blog:detail', kwargs={'pk': self.pk})
-	def increase_views(self):
-		self.views += 1
-		self.save(update_field = ['views'])
-
+	#文章标题
 	title = models.CharField(max_length = 70)
+
+	#文章内容
 	body = models.TextField()
+
+	#文章创建时间
 	create_time = models.DateTimeField()
+
+	#文章修改时间
 	modified_time = models.DateTimeField()
 
 	#文章摘要
@@ -51,7 +50,40 @@ class Post(models.Model):
 
 	#user
 	author = models.ForeignKey(User)
+
+	def __str__(self):
+		return self.title
+
+	# 自定义 get_absolute_url 方法
+    # 记得从 django.urls 中导入 reverse 函数
+	def get_absolute_url(self):
+		return reverse('blog:detail', kwargs={'pk': self.pk})
+
+	def increase_views(self):
+		self.views += 1
+		self.save(update_fields = ['views'])
+
 	class Meta:
 		ordering = ['-create_time']
+  
+    #文章自动生成摘要信息
+    #方法一：复写save方法
+	def save(self,*args,**kwargs):
+    	#如果没有填写摘要
+		if not self.excerpt:
+    		#实例化一个markdown实例，去渲染文章文本
+			md = markdown.Markdown(extensions = [
+    			'markdown.extensions.extra',
+    			'markdown.extensions.codehilite',
+    			])
+    		#首先将markdown文本渲染成html文本，然后去掉html文本中的
+			#html标签，然后取前54个字符为文章摘要
+			self.excerpt = strip_tags(md.convert(self.body))[:54]
+
+		#调用父类save方法，将数据保存至数据库中
+		super(Post,self).save(*args,**kwargs)    
+	#方法二：使用truncatechars模本过滤器
+	#详见index.html中post.body | truncatechars:54
+
 
 
