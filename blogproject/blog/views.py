f6bba9c3f23eff5f742a046d6b70d03588723734
GreaterCode <1967368231@qq.com>
2017-12-20 00:47:36 +0800

    本次主要增加了四个功能
    1、统计各个分类下的文章数
    2、增加了标签功能
    3、订阅功能
    4、文章自动生成目录

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -5,127 +5,159 @@ from django.http import HttpResponse
 from comments.forms import CommentForm
 from .models import Post,Category
 from django.views.generic import ListView,DetailView
-from django.core.paginator import Paginator
-
+from markdown.extensions.toc import TocExtension
+from django.utils.text import slugify
 
 
 
 # Create your views here.
 class IndexView(ListView):
-	model = Post
-	template_name = 'blog/index.html'
-	context_object_name = 'post_list'
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
 
-	#开启分页功能
-	paginate_by = 2
+    #开启分页功能
+    paginate_by = 1
 
-	def get_context_data(self,**kwargs):
-		#获取父类生成的传递给模版的字典
-		context = super().get_context_data(**kwargs)	
-		
-		#从父类的字典中获取paginator、page_obj、is_paginated
-		#paginator：Paginator的一个实例
-		#page_obj:Page的实例
-		#is_paginated:指示是否已经分页
-		paginator = context.get('paginator')
-		page = context.get('page_obj')
-		is_paginated = context.get('is_paginated')
+    def get_context_data(self, **kwargs):
+        """
+        在视图函数中将模板变量传递给模板是通过给 render 函数的 context 参数传递一个字典实现的，
+        例如 render(request, 'blog/index.html', context={'post_list': post_list})，
+        这里传递了一个 {'post_list': post_list} 字典给模板。
+        在类视图中，这个需要传递的模板变量字典是通过 get_context_data 获得的，
+        所以我们复写该方法，以便我们能够自己再插入一些我们自定义的模板变量进去。
+        """
 
-		#调用自己实现的pagination_data获取显示分页的内容
-		pagination_data = self.pagination_data(paginator,page,is_paginated)
+        # 首先获得父类生成的传递给模板的字典。
+        context = super().get_context_data(**kwargs)
 
-		#将分页导航条的模板变量更新到context中
-		context.update(pagination_data)
+        # 父类生成的字典中已有 paginator、page_obj、is_paginated 这三个模板变量，
+        # paginator 是 Paginator 的一个实例，
+        # page_obj 是 Page 的一个实例，
+        # is_paginated 是一个布尔变量，用于指示是否已分页。
+        # 例如如果规定每页 10 个数据，而本身只有 5 个数据，其实就用不着分页，此时 is_paginated=False。
+        # 关于什么是 Paginator，Page 类在 Django Pagination 简单分页：http://zmrenwu.com/post/34/ 中已有详细说明。
+        # 由于 context 是一个字典，所以调用 get 方法从中取出某个键对应的值。
+        paginator = context.get('paginator')
+        page = context.get('page_obj')
+        is_paginated = context.get('is_paginated')
 
-		#返回更新后的context
-		return context
+        # 调用自己写的 pagination_data 方法获得显示分页导航条需要的数据，见下方。
+        pagination_data = self.pagination_data(paginator, page, is_paginated)
 
-	def pagination_data(self,paginator,page,is_paginated):
-		if not is_paginated:
-			#若无分页，则无需显示分页导航条
-			return {}
+        # 将分页导航条的模板变量更新到 context 中，注意 pagination_data 方法返回的也是一个字典。
+        context.update(pagination_data)
 
-		#当前页左边连续的页码号，初值为空
-		left = []
+        # 将更新后的 context 返回，以便 ListView 使用这个字典中的模板变量去渲染模板。
+        # 注意此时 context 字典中已有了显示分页导航条所需的数据。
+        return context
 
-		#当前页右边连续的页码号，初值为空
-		right = []
+    def pagination_data(self, paginator, page, is_paginated):
+        if not is_paginated:
+            # 如果没有分页，则无需显示分页导航条，不用任何分页导航条的数据，因此返回一个空的字典
+            return {}
 
-		#标识第一页页码后面是否需要省略号
-		left_has_more = False
+        # 当前页左边连续的页码号，初始值为空
+        left = []
 
-		#标识最后一页前是否需要省略号
-		right_has_more = False
-    	
-    	#标识是否需要显示第一页
-		first = False
+        # 当前页右边连续的页码号，初始值为空
+        right = []
 
-		#标识是否需要显示最后一页
-		last = False
+        # 标示第 1 页页码后是否需要显示省略号
+        left_has_more = False
 
-		#获得用户当前请求的页码号
-		page_number = page.number
+        # 标示最后一页页码前是否需要显示省略号
+        right_has_more = False
 
-		#获得分页后的总页数
-		total_pages = paginator.num_pages
+        # 标示是否需要显示第 1 页的页码号。
+        # 因为如果当前页左边的连续页码号中已经含有第 1 页的页码号，此时就无需再显示第 1 页的页码号，
+        # 其它情况下第一页的页码是始终需要显示的。
+        # 初始值为 False
+        first = False
 
-		#获得整个分页页码列表，如[1,2,3,4,5]
-		page_range = paginator.page_range
+        # 标示是否需要显示最后一页的页码号。
+        # 需要此指示变量的理由和上面相同。
+        last = False
 
-		if page_number == 1:
-			#获取当前页右边的连续页码号
-			right = page_range[page_number:page_number+1]
-			#如果最右边的页码比倒数第二页小，则需显示省略号
-			if right[-1] < total_pages -1:
-				right_has_more = True
-			
-			#若最右边页码比最后一页还小，则显示最后一页页码
-			if right[-1] < total_pages:
-				last = True
-		elif page_number == total_pages:
-			#如果用户请求最后一页,
-			if (page_number - 3) > 0:
-				left = page_range[page_number - 3:page_number -1]
-			else:
-				left = 0
+        # 获得用户当前请求的页码号
+        page_number = page.number
 
-			if left[0] > 2:
-				left_has_more = True
+        # 获得分页后的总页数
+        total_pages = paginator.num_pages
 
-			if left[0] > 1:
-				first = True
-		else:
-			left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
-			right = page_number[page_number:page_number + 2]
+        # 获得整个分页页码列表，比如分了四页，那么就是 [1, 2, 3, 4]
+        page_range = paginator.page_range
 
-			if right[-1] < total_pages - 1:
-				right_has_more = True
-			if right[-1] < total_pages:
-				last = True
+        if page_number == 1:
+            # 如果用户请求的是第一页的数据，那么当前页左边的不需要数据，因此 left=[]（已默认为空）。
+            # 此时只要获取当前页右边的连续页码号，
+            # 比如分页页码列表是 [1, 2, 3, 4]，那么获取的就是 right = [2, 3]。
+            # 注意这里只获取了当前页码后连续两个页码，你可以更改这个数字以获取更多页码。
+            right = page_range[page_number:page_number + 2]
 
-			if left[0] > 2:
-				left_has_more = True
+            # 如果最右边的页码号比最后一页的页码号减去 1 还要小，
+            # 说明最右边的页码号和最后一页的页码号之间还有其它页码，因此需要显示省略号，通过 right_has_more 来指示。
+            if right[-1] < total_pages - 1:
+                right_has_more = True
 
-			if left[0] > 1:
-				first = True
-		data = {
-		'left':left,
-		'right':right,
-		'left_has_more':left_has_more,
-		'right_has_more':right_has_more,
-		'first':first,
-		'last':last,
+            # 如果最右边的页码号比最后一页的页码号小，说明当前页右边的连续页码号中不包含最后一页的页码
+            # 所以需要显示最后一页的页码号，通过 last 来指示
+            if right[-1] < total_pages:
+                last = True
 
-		}
+        elif page_number == total_pages:
+            # 如果用户请求的是最后一页的数据，那么当前页右边就不需要数据，因此 right=[]（已默认为空），
+            # 此时只要获取当前页左边的连续页码号。
+            # 比如分页页码列表是 [1, 2, 3, 4]，那么获取的就是 left = [2, 3]
+            # 这里只获取了当前页码后连续两个页码，你可以更改这个数字以获取更多页码。
+            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
 
-		return data
+            # 如果最左边的页码号比第 2 页页码号还大，
+            # 说明最左边的页码号和第 1 页的页码号之间还有其它页码，因此需要显示省略号，通过 left_has_more 来指示。
+            if left[0] > 2:
+                left_has_more = True
+
+            # 如果最左边的页码号比第 1 页的页码号大，说明当前页左边的连续页码号中不包含第一页的页码，
+            # 所以需要显示第一页的页码号，通过 first 来指示
+            if left[0] > 1:
+                first = True
+        else:
+            # 用户请求的既不是最后一页，也不是第 1 页，则需要获取当前页左右两边的连续页码号，
+            # 这里只获取了当前页码前后连续两个页码，你可以更改这个数字以获取更多页码。
+            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
+            right = page_range[page_number:page_number + 2]
+
+            # 是否需要显示最后一页和最后一页前的省略号
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+
+            # 是否需要显示第 1 页和第 1 页后的省略号
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+
+        data = {
+            'left': left,
+            'right': right,
+            'left_has_more': left_has_more,
+            'right_has_more': right_has_more,
+            'first': first,
+            'last': last,
+        }
+
+        return data
+
+
+    
 
 
 
 
 
-
-					
+                    
 
 
 
@@ -136,15 +168,15 @@ class IndexView(ListView):
 
 '''
 def index(request):
-	post_list = Post.objects.all().order_by('-create_time')
-	return render(request,'blog/index.html',context={'post_list':post_list})
-	'''
+    post_list = Post.objects.all().order_by('-create_time')
+    return render(request,'blog/index.html',context={'post_list':post_list})
+    '''
 '''
-	return  render(request,'blog/index.html',context ={
-			'title':'我的博客首页',
-			'welcome':'欢迎访问我的博客首页'
+    return  render(request,'blog/index.html',context ={
+            'title':'我的博客首页',
+            'welcome':'欢迎访问我的博客首页'
 
-		})
+        })
 '''
 '''
 def detail(request, pk):
@@ -154,8 +186,8 @@ def detail(request, pk):
     post.increase_views()
     
     #extra:包含很多扩展
-	#codehilite:使得文字语法高亮
-	#toc：自动生成目录
+    #codehilite:使得文字语法高亮
+    #toc：自动生成目录
     post.body = markdown.markdown(post.body,
                                   extensions=[
                                       'markdown.extensions.extra',
@@ -165,43 +197,49 @@ def detail(request, pk):
     form = CommentForm()
     comment_list = post.comment_set.all()
     context = {'post':post,
-    			'form':form,
-    			'comment_list':comment_list
+                'form':form,
+                'comment_list':comment_list
     }
     return render(request, 'blog/detail.html', context=context)
 '''
 class PostDetailView(DetailView):
-	model = Post
-	template_name = 'blog/detail.html'
-	context_object_name = 'post'
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
 
-	#重写get（）方法,以实现对阅读量的递增
-	def get(self,request,*args,**kwargs):
-		response = super(PostDetailView,self).get(request,*args,**kwargs)
+    #重写get（）方法,以实现对阅读量的递增
+    def get(self,request,*args,**kwargs):
+        response = super(PostDetailView,self).get(request,*args,**kwargs)
 
-		#将阅读量+1
-		self.object.increase_views()
-		return response
+        #将阅读量+1
+        self.object.increase_views()
+        return response
 
-	#重写get_object()以实现对post的body进行渲染
-	def get_object(self,queryset = None):
-		post = super(PostDetailView,self).get_object(queryset=None)
-		post.body = markdown.markdown(post.body,extensions = [
-			'markdown.extensions.extra',
-			'markdown.extensions.codehilite',
-			'markdown.extensions.toc',
-			])
-		return post
+    #重写get_object()以实现对post的body进行渲染
+    def get_object(self,queryset = None):
+        post = super(PostDetailView,self).get_object(queryset=None)
+        
+        #在页面的任何地方都可以插入目录
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            'markdown.extensions.toc',
+            #美化标题的锚点URL
+            TocExtension(slugify=slugify),
+        ])
+        post.body = md.convert(post.body)
+        post.toc = md.toc
+        return post
 
-	def get_context_data(self,**kwargs):
-		context = super(PostDetailView,self).get_context_data(**kwargs)
-		form = CommentForm()
-		comment_list = self.object.comment_set.all()
-		context.update({
-			'form':form,
-			'comment_list':comment_list
-			})
-		return context
+    def get_context_data(self,**kwargs):
+        context = super(PostDetailView,self).get_context_data(**kwargs)
+        form = CommentForm()
+        comment_list = self.object.comment_set.all()
+        context.update({
+            'form':form,
+            'comment_list':comment_list
+            })
+        return context
 
 
 
@@ -217,24 +255,34 @@ def archives(request, year, month):
     return render(request, 'blog/index.html', context={'post_list': post_list})
  '''
 class ArchivesView(ListView):
- 	model = Post
- 	template_name = 'blog/index.html'
- 	context_object_name = 'post_list'
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
 
- 	def get_queryset(self):
- 		year = self.kwargs.get('year')
- 		month = self.kwargs.get('month')
- 		return super(ArchivesView,self).get_queryset().filter(create_time__year=year,
- 															create_time__month=month)
+    def get_queryset(self):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        return super(ArchivesView,self).get_queryset().filter(create_time__year=year,
+                                                            create_time__month=month)
 
 
 '''
 def category(request,pk):
-	cate =  get_object_or_404(Category,pk=pk)
-	post_list = Post.objects.filter(category=cate).order_by('-create_time')
-	return render(request,'blog/index.html',context = {'post_list':post_list})
+    cate =  get_object_or_404(Category,pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-create_time')
+    return render(request,'blog/index.html',context = {'post_list':post_list})
 '''
 class CategoryView(IndexView):
-	def get_queryset(self):
-		cate = get_object_or_404(Category,pk=self.kwargs.get('pk'))
-		return super(CategoryView,self).get_queryset().filter(category = cate)
+    def get_queryset(self):
+        cate = get_object_or_404(Category,pk=self.kwargs.get('pk'))
+        return super(CategoryView,self).get_queryset().filter(category = cate)
+
+class TagView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        tag = get_object_or_404(Tag,pk=self.kwargs.get('pk'))
+        return  super(TagView,self).get_queryset().filter(tags = tag)
+
